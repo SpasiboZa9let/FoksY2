7eb7aeb8cd86425382ad26f8c92b0e7a624777d8
Иван <foxneversleep@gmail.com>
2025-07-07 11:29:46 +0300

    Add files via upload

--- js/foxy/core/state.js
+++ js/foxy/core/state.js
@@ -1,1 +1,28 @@
 
+// js/foxy/state.js
+
+// фильтр повторов
+export let lastInput = "";
+export function setLastInput(val) {
+  lastInput = val;
+}
+
+// сейчас статично, но можно менять
+export let foxyMood = "neutral";
+export function setFoxyMood(mood) {
+  foxyMood = mood;
+}
+export let lastIntent = null;
+export function setLastIntent(val) {
+  lastIntent = val;
+}
+
+export let lastService = null;
+export function setLastService(val) {
+  lastService = val;
+}
+export let lastReplyType = null;
+export function setLastReplyType(t) {
+  lastReplyType = t;
+}
+
