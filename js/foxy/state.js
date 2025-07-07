dee538ef7834ae5123eb39691f8803f3d186c1ca
Иван <foxneversleep@gmail.com>
2025-07-07 09:33:02 +0300

    Update state.js

--- js/foxy/state.js
+++ js/foxy/state.js
@@ -1,4 +1,17 @@
-// Контекст для диалога
+
+// js/foxy/state.js
+
+// фильтр повторов
+export let lastInput = "";
+export function setLastInput(val) {
+  lastInput = val;
+}
+
+// сейчас статично, но можно менять
+export let foxyMood = "neutral";
+export function setFoxyMood(mood) {
+  foxyMood = mood;
+}
 export let lastIntent = null;
 export function setLastIntent(val) {
   lastIntent = val;
