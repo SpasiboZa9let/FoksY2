484bbb81b1a9a64faf2e5048be777cf797e37d55
Иван <foxneversleep@gmail.com>
2025-07-07 12:01:33 +0300

    Update mainHandler.js

--- js/foxy/handlers/mainHandler.js
+++ js/foxy/handlers/mainHandler.js
@@ -1,1 +1,45 @@
+// foxy/handlers/mainHandler.js
+
+import { matchIntent } from "../core/intents.js";
+import { emoji } from "../core/services.js";
+import { addMessage, clearButtons } from "../ui/dom.js";
+import { setLastInput, lastInput, setLastIntent } from "../core/state.js";
+
+// ветки
+import { handleDesign } from "./design.js";
+import { handleService, handleServiceDetail } from "./services.js";
+import { handleMood } from "./mood.js";
+import { handleSmalltalk } from "./smalltalk.js";
+
+export function handleUserInput(message) {
+  clearButtons();
+
+  const input = message.trim();
+  if (!input || input.toLowerCase() === lastInput) return;
+  setLastInput(input.toLowerCase());
+
+  addMessage(`Вы: ${message}`);
+
+  // определить интент
+  const intent = matchIntent(input.toLowerCase());
+  setLastIntent(intent);
+
+  // маршрутизация по интентам
+  switch (intent) {
+    case "design":
+      return handleDesign(input);
+
+    case "service":
+      return handleService(input);
+
+    case "serviceDetail":
+      return handleServiceDetail(input);
+
+    case "mood":
+      return handleMood(input);
+
+    default:
+      return handleSmalltalk(intent, input);
+  }
+}
 
