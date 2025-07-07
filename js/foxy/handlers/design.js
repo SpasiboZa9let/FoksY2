3c5ecf5467c27b819ef35538134590b081ae33ca
Иван <foxneversleep@gmail.com>
2025-07-07 12:02:05 +0300

    Update design.js

--- js/foxy/handlers/design.js
+++ js/foxy/handlers/design.js
@@ -1,1 +1,12 @@
+// foxy/handlers/design.js
+
+import { addMessage } from "../ui/dom.js";
+import { randomReply, emoji } from "../core/services.js";
+import { showTrendyOptions } from "../ui/ui.js"; // можно перенести в отдельный ui/design.js при росте
+
+export function handleDesign() {
+  addMessage(`${emoji()} Вот вдохновляющие идеи для дизайна ноготков 💅`);
+  addMessage(randomReply("design"), true);
+  showTrendyOptions();
+}
 
