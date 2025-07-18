87833ebb555ff5eaa9ac93b08dea922c9940368a
Иван <foxneversleep@gmail.com>
2025-07-18 18:52:55 +0300

    Delete js directory

--- js/foxy/handlers/booking.js
+++
@@ -1,9 +0,0 @@
-export async function sendBooking({ name, service, date }) {
-  console.log(`[sendBooking] ➤`, { name, service, date });
-  return { success: true, mocked: true };
-}
-
-export async function requestVisitConfirmation(name, date) {
-  console.log(`[requestVisitConfirmation] ➤`, { name, date });
-  return { success: true, mocked: true };
-}
